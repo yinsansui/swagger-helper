e5b5b31bb4fdc21a01ead7f80f1d938948f846d1
yinxy <yinxy@abcyun.cn>
2022-08-12 00:49:26 +0800

    通过内部类的方式实现懒加载的单例模式

--- src/main/java/com/sharedaka/processor/ProcessorHolder.java
+++ src/main/java/com/sharedaka/processor/ProcessorHolder.java
@@ -7,64 +7,35 @@ import com.sharedaka.processor.business.SwaggerApiModelProcessor;
 /**
  * Swagger Helper现提供三个功能，其中两个与类相关，一个与方法相关，三个功能对应三个处理器
  * 1. Class相关：
- *     - 实体类生成@ApiModel相关注解   - SwaggerApiModelProcessor
- *     - Controller类生成@Api相关注解 - SwaggerApiControllerProcessor
+ * - 实体类生成@ApiModel相关注解   - SwaggerApiModelProcessor
+ * - Controller类生成@Api相关注解 - SwaggerApiControllerProcessor
  * 2. Method相关：
- *     - 生成对应Controller注释       - SwaggerApiMethodProcessor
+ * - 生成对应Controller注释       - SwaggerApiMethodProcessor
  * 该类负责对处理器进行管理
- *
- * */
+ */
 public class ProcessorHolder {
 
-    public static SwaggerApiControllerProcessor swaggerApiControllerProcessor;
-
-    public static final String swaggerApiControllerProcessorLock = "";
-
-
-    public static SwaggerApiMethodProcessor swaggerApiMethodProcessor;
-
-    public static final String swaggerApiMethodProcessorLock = "";
-
-    private static SwaggerApiModelProcessor swaggerApiModelProcessor;
-
-    public static final String swaggerApiModelProcessorLock = "";
+    private static final class SwaggerApiControllerProcessorHolder {
+        static final SwaggerApiControllerProcessor SWAGGER_API_CONTROLLER_PROCESSOR = new SwaggerApiControllerProcessor();
+    }
 
     public static SwaggerApiControllerProcessor getSwaggerApiControllerProcessor() {
-        if (swaggerApiControllerProcessor != null) {
-            return swaggerApiControllerProcessor;
-        } else {
-            synchronized (swaggerApiControllerProcessorLock) {
-                if (swaggerApiControllerProcessor == null) {
-                    swaggerApiControllerProcessor = new SwaggerApiControllerProcessor();
-                }
-            }
-            return swaggerApiControllerProcessor;
-        }
+        return SwaggerApiControllerProcessorHolder.SWAGGER_API_CONTROLLER_PROCESSOR;
+    }
+
+    private static final class SwaggerApiMethodProcessorHolder {
+        static final SwaggerApiMethodProcessor SWAGGER_API_METHOD_PROCESSOR = new SwaggerApiMethodProcessor();
     }
 
     public static SwaggerApiMethodProcessor getSwaggerApiMethodProcessor() {
-        if (swaggerApiMethodProcessor != null) {
-            return swaggerApiMethodProcessor;
-        } else {
-            synchronized (swaggerApiMethodProcessorLock) {
-                if (swaggerApiMethodProcessor == null) {
-                    swaggerApiMethodProcessor = new SwaggerApiMethodProcessor();
-                }
-            }
-            return swaggerApiMethodProcessor;
-        }
+        return SwaggerApiMethodProcessorHolder.SWAGGER_API_METHOD_PROCESSOR;
+    }
+
+    private static final class SwaggerApiModelProcessorHolder {
+        static final SwaggerApiModelProcessor SWAGGER_API_MODEL_PROCESSOR = new SwaggerApiModelProcessor();
     }
 
     public static SwaggerApiModelProcessor getSwaggerApiModelProcessor() {
-        if (swaggerApiModelProcessor != null) {
-            return swaggerApiModelProcessor;
-        } else {
-            synchronized (swaggerApiModelProcessorLock) {
-                if (swaggerApiModelProcessor == null) {
-                    swaggerApiModelProcessor = new SwaggerApiModelProcessor();
-                }
-            }
-            return swaggerApiModelProcessor;
-        }
+        return SwaggerApiModelProcessorHolder.SWAGGER_API_MODEL_PROCESSOR;
     }
 }
